ea05ef7e703f822e4dc7d36d50616e6cc56ea44c
anildalar <dollorinfotech@gmail.com>
2024-05-09 10:29:07 +0530

    Seeding Example

---
+++ database/seeders/StudentSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\StudentModel;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class StudentSeeder extends Seeder
+{
+    //1 Propery
+
+
+
+    //2. Constructor
+
+
+    //3. Methods
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        // User::factory(10)->create();
+
+        DB::table('students')->insert([
+            'name' => 'yash',
+            'surname' => 'mali',
+            'fname' => 'bhwarlalji',
+            'mname' => 'pinkiji',
+            'mobno' => '798798987968',
+        ]);
+    }
+}
